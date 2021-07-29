cabe495a2f9a7d85036db21631bc5c1046ec8cf7
lautaroLD <lauttyd@gmail.com>
2021-07-29 13:24:49 -0300

    agrego funcion de recargar caundo falla la conexion

--- config.js
+++ config.js
@@ -1,8 +1,20 @@
 const audioElement = document.getElementById("audioId")
-
+const reconect = () => {
+        setTimeout(() => {
+            let errorCode = audioElement.error.code
+            console.log(errorCode)
+            if (errorCode) {
+                document.location.reload();    
+            }
+        }, 1000 * 3);
+}
+const vuelta = () => {
+    let est = document.getElementById("audioId").contentWindow
+    est.location.reload()
+}
 const recargar = () => {
     console.log()
-    document.location.reload();
+    document.location.reload(audioElement);
 }
 const empezarParar = () => {
     let btn = document.getElementById("bx-btn-play-pause")
@@ -13,6 +25,7 @@ const empezarParar = () => {
         audioElement.pause()
         btn.className = "bx bx-play"
     }
+
 }
 const mutear = () => {
     let btn = document.getElementById("bx-btn-muteOnOff")
@@ -20,9 +33,8 @@ const mutear = () => {
         audioElement.muted = false
         btn.className = "bx bx-volume-full"
     } else {
-        audioElement.muted = true 
+        audioElement.muted = true
         btn.className = "bx bx-volume-mute"
     }
 }
-// const conectado = (e) => { console.log(e) }
 
