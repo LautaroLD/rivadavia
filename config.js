081cedf09278d1bcd3a34382580ba831fe43b664
LautaroLD <lauttyd@gmail.com>
2021-09-07 20:27:34 -0300

    creo elemnto de carga

--- config.js
+++ config.js
@@ -1,12 +1,36 @@
 const audioElement = document.getElementById("audioId")
+const reconect = document.getElementById("reconect-container")
+// if (audioElement.readyState == 0) {
+//     console.log("0")
+// }
+// if (audioElement.readyState == 1) {
+//     console.log("1")
+// }
+// if (audioElement.readyState == 2) {
+//     console.log("2")
+// }
+// if (audioElement.readyState == 3) {
+//     console.log("3")
+// }
+// if (audioElement.readyState == 4) {
+//     console.log("4")
+// }
+audioElement.addEventListener('loadstart', (ev) => {
+    reconect.className = "reconect-container"
+})
 
+audioElement.addEventListener('stalled', (ev) => {
+    reconect.className = "reconect-container"
+        audioElement.load()
+})
 audioElement.addEventListener('canplay', (ev) => {
-    console.log(ev)
+    reconect.className = "reconect-container-disable"
     audioElement.play()
 })
-audioElement.addEventListener('error', (ev) => {
-    audioElement.load()
-})
+
+// audioElement.addEventListener('error', (ev) => {
+//     audioElement.load()
+// })
 const controles = (btn) => {
     let btn_play_pause = document.getElementById("bx-btn-play-pause")
     let btn_mute = document.getElementById("bx-btn-muteOnOff")
