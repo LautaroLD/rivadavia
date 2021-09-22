46ee6b19b0045d9258573d3a77a86e22140b187c
LautaroLD <lauttyd@gmail.com>
2021-09-22 20:10:57 -0300

    cambio a modulos ECMAScript 6 y cambio de funcionamiento de los EventListener

---
+++ config.mjs
@@ -0,0 +1,8 @@
+import * as event from "./eventListener.js"
+
+event.navigator_change()
+event.audio_stalled()
+event.audio_loadstart()
+event.audio_loadeddata()
+event.audio_paused()
+event.audio_playing()
