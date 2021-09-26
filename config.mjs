a42c0932dbed494f7579e377e230f4b940a7f95b
LautaroLD <lauttyd@gmail.com>
2021-09-25 21:13:21 -0300

    agrego modulo para las variables

--- config.mjs
+++ config.mjs
@@ -1,8 +1,7 @@
-import * as event from "./eventListener.js"
-
-event.navigator_change()
-event.audio_stalled()
-event.audio_loadstart()
-event.audio_loadeddata()
-event.audio_paused()
-event.audio_playing()
+import * as eventListener from "./eventListener.js"
+eventListener.navigator_change()
+eventListener.audio_stalled()
+eventListener.audio_loadstart()
+eventListener.audio_loadeddata()
+eventListener.audio_paused()
+eventListener.audio_playing()
--- eventListener.js
+++ eventListener.js
@@ -1,63 +1,59 @@
-let reconect = document.getElementById("reconect-container")
-let audioElement = document.getElementById("audioId")
-let btn_play_pause = document.getElementById("bx-btn-play-pause")
-let btn_mute = document.getElementById("bx-btn-muteOnOff")
-let btn_reset = document.getElementById("bx-btn-reset")
-
-btn_play_pause.addEventListener("click", () => {
-    if (btn_play_pause.className === "bx bx-play") {
-        audioElement.play()
-        btn_play_pause.className = "bx bx-pause"
+import * as variables from "./variables.js"
+variables.btn_play_pause.addEventListener("click", () => {
+    if (variables.btn_play_pause.className === "bx bx-play") {
+        variables.audioElement.play()
+        variables.btn_play_pause.className = "bx bx-pause"
     } else {
-        audioElement.pause()
-        btn_play_pause.className = "bx bx-play"
+        variables.audioElement.pause()
+        variables.btn_play_pause.className = "bx bx-play"
     }
 })
-btn_mute.addEventListener("click", () => {
-    if (audioElement.muted) {
-        audioElement.muted = false
-        btn_mute.className = "bx bx-volume-full"
+variables.btn_mute.addEventListener("click", () => {
+    if (variables.audioElement.muted) {
+        variables.audioElement.muted = false
+        variables.btn_mute.className = "bx bx-volume-full"
     } else {
-        audioElement.muted = true
-        btn_mute.className = "bx bx-volume-mute"
+        variables.audioElement.muted = true
+        variables.btn_mute.className = "bx bx-volume-mute"
     }
 })
-btn_reset.addEventListener("click", () => {
-    audioElement.load()
-    btn_play_pause.className = "bx bx-play"
+variables.btn_reset.addEventListener("click", () => {
+    variables.audioElement.load()
+    variables.btn_play_pause.className = "bx bx-play"
 })
-audioElement = new Audio("https://playerservices.streamtheworld.com/api/livestream-redirect/RIVADAVIAAAC.aac")
 
 export const audio_playing = () => {
-    audioElement.addEventListener("playing", () => {
-        btn_play_pause.className = "bx bx-pause"
+    variables.audioElement.addEventListener("playing", () => {
+        variables.btn_play_pause.className = "bx bx-pause"
     })
 }
 export const navigator_change = () => {
     navigator.connection.addEventListener('change', () => {
+        console.log("change")
         location.reload()
     })
 }
 export const audio_paused = () => {
-    audioElement.addEventListener("pause", () => {
-        btn_play_pause.className = "bx bx-play"
+    variables.audioElement.addEventListener("pause", () => {
+        variables.btn_play_pause.className = "bx bx-play"
     })
 }
 export const audio_loadeddata = () => {
-    audioElement.addEventListener("loadeddata", () => {
-        reconect.className = "reconect-container-disable"
-        audioElement.play()
+    variables.audioElement.addEventListener("canplay", () => {
+        variables.reconect.className = "reconect-container-disable"
+        variables.audioElement.play()
     })
 }
 export const audio_loadstart = () => {
-    audioElement.addEventListener("loadstart", () => {
-        reconect.className = "reconect-container"
+    variables.audioElement.addEventListener("loadstart", () => {
+        variables.reconect.className = "reconect-container"
     })
 }
 export const audio_stalled = () => {
-    audioElement.addEventListener("stalled", () => {
-        reconect.lastElementChild.innerText = "Reconectando..."
-        reconect.className = "reconect-container"
-        audioElement.load()
+    variables.audioElement.addEventListener("error", () => {
+        variables.reconect.lastElementChild.innerText = "Reconectando..."
+        variables.reconect.className = "reconect-container"
+        console.log("error")
+        variables.audioElement.load()
     })
 }
